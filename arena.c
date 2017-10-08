f8d0a2bf523b74adf744e3420a12bccb06266c2a
DarknessCatt <matheus.limacunha@hotmail.com>
2017-10-08 17:22:18 -0300

    Umas mudanças
    legais

--- arena.c
+++ arena.c
@@ -1,10 +1,21 @@
-Arena *init_arena() {
-	Celula arena[11][11];
+#include <stdio.h>
+
+#define GRID 11
+
+static Celula[][] *init_arena() {
+	Celula arena[GRID][GRID];
 	int i,j;
-	for(i=1;i<11;i++) {
-		for(j=1; j<11; j++) {
+	for(i=1;i<GRID;i++) {
+		for(j=1; j<GRID; j++) {
 			arena[i][j].terreno = rand() % 4;
 			arena[i][j].cristais = rand() % 5;
 		}
 	}
+	return arena;
+}
+
+int main() {
+	Arena campo;
+	campo->arena = init_arena();
+	campo->time = 0;
 }
