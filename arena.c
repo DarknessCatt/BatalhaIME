4e0133009a38701be2d4573279755b1f98289ef5
Eduwzin <31973809+Eduwzin@users.noreply.github.com>
2017-10-09 22:18:17 -0300

    Funcao Registro
    Teste pra funcao registro. Só tirar o que ta comentado dps pra testar, ele cria uma máquina e coloca no vetor exercitos.

--- arena.c
+++ arena.c
@@ -3,8 +3,17 @@
 #include "arena.h"
 #define GRID 21
 
+/*INSTR programa[] = {
+  {PUSH, 3},
+  {PUSH, 6},
+  {CALL, 5},
+  {PRN,  0},
+  {END,  0},
+  {ADD,  0},
+  {RET,  0}
+};*/
+
 Arena* init_arena() {
-	printf("teste");
 	Arena *arena = (Arena*)malloc(sizeof(Arena));
 	maquinas = 0;
 	int i,j;
@@ -22,9 +31,15 @@ Arena* init_arena() {
 void registro(Maquina *maq) {
 	exercito[maquinas] = *maq;
 	maquinas++;
+	/*printf("Maquinas:%d\n",maquinas);
+	exercito[0].cristais = 217;
+	printf("%d",exercito[0].cristais);*/
 }
+
 
 int main() {
 	Arena *arena = init_arena();
+	Maquina *maq = cria_maquina(programa);
+	registro(maq);
 	printf("%d",arena->cell[1][1].cristais);
 }
